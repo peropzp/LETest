c522243b86ed05a7be54600a08670a8e0c80477c
peropzp <154701244+peropzp@users.noreply.github.com>
2023-12-25 10:51:15 +0100

    Create link_telematics_wialon.sql

---
+++ link_telematics_wialon.sql
@@ -0,0 +1,13 @@
+with link as (select t.serialnumber, w.unit_id, 
+sum((abs(w.gpsLatitude-t.gpsLatitude) + abs(w.gpsLongitude - t.gpsLongitude)))/count(*) as dst,
+count(*) as cnt
+from `test1.telematics` as t, `test1.wialon` as w
+where round(w.gpsLatitude * 100000) = round(t.gpsLatitude * 100000)
+and round(w.gpsLongitude * 100000) = round(t.gpsLongitude * 100000)
+and abs(datetime_diff(w.datetime, t.datetime, SECOND)) < 120
+and w.speed = 0
+group by t.serialnumber, w.unit_id)
+select l1.serialnumber, l1.unit_id
+from link as l1
+where l1.cnt = (select max(l2.cnt) from link as l2 where l1.serialnumber = l2.serialnumber )
+order by l1.serialnumber, cnt desc
