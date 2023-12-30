124e3df109b06d76545b3954190cb247a4854415
peropzp <154701244+peropzp@users.noreply.github.com>
2023-12-30 10:14:23 +0100

    Added @@dataset_id

--- 5-Populate JoinedData.sql
+++ 5-Populate JoinedData.sql
@@ -1,14 +1,17 @@
-delete from LETestDataset1.joinedData where 1=1;
+set @@dataset_project_id = 'letest-409019';
+set @@dataset_id = 'LETestDataset';
 
-insert into LETestDataset1.joinedData
+delete from joinedData where 1=1;
+
+insert into joinedData
 with  
-  f as (select timestamp as ft, machineId from LETestDataset1.fendt order by machineId, ft),
-  t as (select date_diff(datetime, timestamp('1970-01-01 00:00:00'), second) as tt, SerialNumber as machineId from LETestDataset1.telematics order by machineId, tt),
-  w as (select date_diff(datetime, timestamp('1970-01-01 00:00:00'), second) as wt, unit_id from LETestDataset1.wialon order by unit_id, wt),
+  f as (select timestamp as ft, machineId from fendt order by machineId, ft),
+  t as (select date_diff(datetime, timestamp('1970-01-01 00:00:00'), second) as tt, SerialNumber as machineId from telematics order by machineId, tt),
+  w as (select date_diff(datetime, timestamp('1970-01-01 00:00:00'), second) as wt, unit_id from wialon order by unit_id, wt),
   mfwjoined as (
     select f.machineid, f.ft, w.wt, w.unit_id
     from f
-    inner join LETestDataset1.MachineIdLink as fw on fw.machineId = cast(f.machineId as STRING)
+    inner join MachineIdLink as fw on fw.machineId = cast(f.machineId as STRING)
     inner join w on w.unit_id = fw.unit_id and round(w.wt / 10) = round(f.ft / 10)),
   fwjoin as (
     select mfwjoined.machineId, mfwjoined.unit_id, mfwjoined.wt, max(mfwjoined.ft) as ft
@@ -18,7 +21,7 @@ with
   mtwjoined as (
     select t.machineid, t.tt, w.wt, w.unit_id
     from t
-    inner join LETestDataset1.MachineIdLink as fw on fw.machineId = cast(t.machineId as STRING)
+    inner join MachineIdLink as fw on fw.machineId = cast(t.machineId as STRING)
     inner join w on w.unit_id = fw.unit_id and round(w.wt / 10) = round(t.tt / 10)),
   twjoin as (
     select mtwjoined.machineId, mtwjoined.unit_id, mtwjoined.wt, max(mtwjoined.tt) as tt
@@ -71,7 +74,7 @@ select
   f.GearOilFilter ,
   f.WHEEL_SLIP ,
   f.WORK_ON 
-from LETestDataset1.fendt as f, LETestDataset1.wialon as w, fwjoin
+from fendt as f, wialon as w, fwjoin
 where f.machineId = fwjoin.machineID and f.timestamp = fwjoin.ft 
 and w.unit_id = fwjoin.unit_id and w.datetime = cast( PARSE_DATETIME('%s', CAST(TRUNC(fwjoin.wt) AS STRING)) as timestamp)
 
@@ -122,7 +125,6 @@ select
   null ,
   null ,
   null 
-from LETestDataset1.telematics as t, LETestDataset1.wialon as w, twjoin
+from telematics as t, wialon as w, twjoin
 where t.SerialNumber = twjoin.machineID and date_diff(t.datetime, timestamp('1970-01-01 00:00:00'), SECOND) = twjoin.tt 
 and w.unit_id = twjoin.unit_id and w.datetime = cast( PARSE_DATETIME('%s', CAST(TRUNC(twjoin.wt) AS STRING)) as timestamp);
-
